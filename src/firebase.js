c94f4f6390f88b8fd9916491c0cbffb5bd8835e1
khanhduy <tranhakhanhduy2001@gmail.com>
2022-06-16 21:39:19 +0700

    install enviroment, code ui  header

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxLNQP1wQtANxiKge84Okz-6ePtlp_U9Q",
+  authDomain: "slackclone-d3bea.firebaseapp.com",
+  projectId: "slackclone-d3bea",
+  storageBucket: "slackclone-d3bea.appspot.com",
+  messagingSenderId: "575920230609",
+  appId: "1:575920230609:web:d74c57c12d8b60e40735f0",
+};
